5347b1aab95c14b055ae2b90a8eea3c655d4b923
NIRAJ CHOWDHARY <chowdharyniraj97@gmail.com>
2020-02-18 18:20:44 -0500

    built auction.sol

--- contracts/Auction.sol
+++ contracts/Auction.sol
@@ -1,24 +1,41 @@
-pragma solidity ^0.5.0;
+pragma solidity >=0.4.22 <0.6.0;
 
-contract Auction {
+contract SimpleAuction {
+    // Parameters of the auction. Times are either
+    // absolute unix timestamps (seconds since 1970-01-01)
+    // or time periods in seconds.
     address payable public beneficiary;
+    uint public auctionEndTime;
 
-    // Current state of the auction. You can create more variables if needed
-
+    // Current state of the auction.
     address public highestBidder;
     uint public highestBid;
 
     // Allowed withdrawals of previous bids
-
     mapping(address => uint) pendingReturns;
 
+    // Set to true at the end, disallows any change.
+    // By default initialized to `false`.
     bool ended;
+
+    // Events that will be emitted on changes.
     event HighestBidIncreased(address bidder, uint amount);
     event AuctionEnded(address winner, uint amount);
-    // Constructor
 
-    constructor() public {
-        beneficiary = msg.sender;
+    // The following is a so-called natspec comment,
+    // recognizable by the three slashes.
+    // It will be shown when the user is asked to
+    // confirm a transaction.
+
+    /// Create a simple auction with `_biddingTime`
+    /// seconds bidding time on behalf of the
+    /// beneficiary address `_beneficiary`.
+    constructor(
+        uint _biddingTime,
+        address payable _beneficiary
+    ) public {
+        beneficiary = _beneficiary;
+        auctionEndTime = now + _biddingTime;
     }
 
     /// Bid on the auction with the value sent
@@ -26,41 +43,82 @@ contract Auction {
     /// The value will only be refunded if the
     /// auction is not won.
     function bid() public payable {
+        // No arguments are necessary, all
+        // information is already part of
+        // the transaction. The keyword payable
+        // is required for the function to
+        // be able to receive Ether.
 
+        // Revert the call if the bidding
+        // period is over.
+        require(
+            now <= auctionEndTime,
+            "Auction already ended."
+        );
 
-        // TODO If the bid is not higher than highestBid, send the
-        // money back. Use "require"
+        // If the bid is not higher, send the
+        // money back.
         require(
             msg.value > highestBid,
             "There already is a higher bid."
         );
-        // TODO update state
 
-        // TODO store the previously highest bid in pendingReturns. That bidder
-        // will need to trigger withdraw() to get the money back.
-        // For example, A bids 5 ETH. Then, B bids 6 ETH and becomes the highest bidder. 
-        // Store A and 5 ETH in pendingReturns. 
-        // A will need to trigger withdraw() later to get that 5 ETH back.
-
-        // Sending back the money by simply using
-        // highestBidder.send(highestBid) is a security risk
-        // because it could execute an untrusted contract.
-        // It is always safer to let the recipients
-        // withdraw their money themselves.
-        
+        if (highestBid != 0) {
+            // Sending back the money by simply using
+            // highestBidder.send(highestBid) is a security risk
+            // because it could execute an untrusted contract.
+            // It is always safer to let the recipients
+            // withdraw their money themselves.
+            pendingReturns[highestBidder] += highestBid;
+        }
+        highestBidder = msg.sender;
+        highestBid = msg.value;
+        emit HighestBidIncreased(msg.sender, msg.value);
     }
 
     /// Withdraw a bid that was overbid.
     function withdraw() public returns (bool) {
+        uint amount = pendingReturns[msg.sender];
+        if (amount > 0) {
+            // It is important to set this to zero because the recipient
+            // can call this function again as part of the receiving call
+            // before `send` returns.
+            pendingReturns[msg.sender] = 0;
 
-        // TODO send back the amount in pendingReturns to the sender. Try to avoid the reentrancy attack. Return false if there is an error when sending
+            if (!msg.sender.send(amount)) {
+                // No need to call throw here, just reset the amount owing
+                pendingReturns[msg.sender] = amount;
+                return false;
+            }
+        }
+        return true;
     }
 
     /// End the auction and send the highest bid
     /// to the beneficiary.
     function auctionEnd() public {
-        // TODO make sure that only the beneficiary can trigger this function. Use "require"
+        // It is a good guideline to structure functions that interact
+        // with other contracts (i.e. they call functions or send Ether)
+        // into three phases:
+        // 1. checking conditions
+        // 2. performing actions (potentially changing conditions)
+        // 3. interacting with other contracts
+        // If these phases are mixed up, the other contract could call
+        // back into the current contract and modify the state or cause
+        // effects (ether payout) to be performed multiple times.
+        // If functions called internally include interaction with external
+        // contracts, they also have to be considered interaction with
+        // external contracts.
 
-        // TODO send money to the beneficiary account. Make sure that it can't call this auctionEnd() multiple times to drain money
+        // 1. Conditions
+        require(now >= auctionEndTime, "Auction not yet ended.");
+        require(!ended, "auctionEnd has already been called.");
+
+        // 2. Effects
+        ended = true;
+        emit AuctionEnded(highestBidder, highestBid);
+
+        // 3. Interaction
+        beneficiary.transfer(highestBid);
     }
 }
